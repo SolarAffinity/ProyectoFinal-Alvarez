579238761fc830ce9f9f7203adad4f2086cca01e
SolarAffinity <solaraffinityycontacto@gmail.com>
2025-10-22 23:17:25 -0300

    chore: config Vercel + Vite y rewrites SPA

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // Quita base si publicas en dominio raíz de Vercel:
+  // base: '/',
+  build: {
+    outDir: 'dist'
+  }
 })
